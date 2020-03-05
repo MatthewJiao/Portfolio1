560999582ce5d0648ce5b36a827662313d002ec2
Matthew Jiao <jiaomatthew222@gmail.com>
2020-03-05 18:25:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function () {
+	$('.menu-toggler').on('click', function () {
+		$(this).toggleClass('open');
+		$('.top-nav').toggleClass('open');
+	});
+
+
+	$('.top-nav .nav-link').on('click', function () {
+		$('.menu-toggler').removeClass('open');
+		$('.top-nav').removeClass('open');
+	});
+
+	$('nav a[href*="#"]').on('click', function () {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 100
+		}, 1000);
+	});
+
+	$('#up').on('click', function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+	AOS.init({
+		easing: 'ease',
+		duration: 1800
+	});
+
+
+});
+
+/*
+document.getElementById('cta').addEventListener('click', 
+	function(){
+		document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', 
+	function(){
+		document.querySelector('.bg-modal').style.display = 'none';
+	});
+*/
